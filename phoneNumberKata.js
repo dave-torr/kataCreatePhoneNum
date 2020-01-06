7d809a6dbb4676dc8c1100995f292d5b90fb391b
djjabbua <djjabbua@gmail.com>
2020-01-06 09:36:03 -0500

    Add files via upload

---
+++ phoneNumberKata.js
@@ -0,0 +1,5 @@
+function createPhoneNumber(numbers){
+      console.log(`(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`)
+    }
+
+   //https://codepen.io/dave-torr/pen/oNgoPXb codepen tests: a more elegant solution is sure to exist, maybe creating and printing an array or var? 
